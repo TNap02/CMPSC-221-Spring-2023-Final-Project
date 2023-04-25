d55ea649e8f2888abe7208017c49193621f3a700
napier <napier@localhost>
2023-04-25 13:54:39 -0400

    Adding Snake Game

---
+++ CMPSC221FinalProject/src/java/snake/SnakeFrame.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package snake;
+import javax.swing.JFrame;
+public class  SnakeFrame extends JFrame {
+    SnakeFrame(){
+        this.add(new SnakePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+        
+    }
+}
