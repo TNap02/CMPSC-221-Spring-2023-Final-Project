447c47b6a23163479f2b6ff22b62d6f18403bf14
napier <napier@localhost>
2023-04-27 14:01:15 -0400

    Adding Snake Game V2

--- CMPSC221FinalProject/src/java/snake/SnakePanel.java
+++ CMPSC221FinalProject/src/java/snake/SnakePanel.java
@@ -1,6 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ * CMPSC 221 Final Project
+ * SnakePanel.java
+ * Purpose: Panel which runs the snake game
+ *
+ * @author Tristan Napier
+ * @version 1.0 4/25/23
  */
 package snake;
 
@@ -11,22 +15,37 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class SnakePanel extends JPanel implements ActionListener {
-
-    static final int S_Width = 1200;
-    static final int S_Height = 600;
-    static final int Game_unit_size = 50;
+    //game board size
+    static int S_Width = 1200;
+    static int S_Height = 600;
+    static int Game_unit_size = 50;
+    
     Timer timer;
     Random random;
-    int foodEAten;
+    int bodylength = 2;
     int foodX;
     int foodY;
-    int bodylength = 2;
+    
     boolean game_flag = false;
     char dir = 'R';
-    static final int DELAY = 160;
+    //static final int DELAY = 50;
     static final int G_Size = (S_Width * S_Height) / (Game_unit_size * Game_unit_size);
     final int x_snake[] = new int[G_Size];
     final int y_snake[] = new int[G_Size];
+   
+    //gametimer stuff
+    GameTimer gameTimer = new GameTimer();
+    
+    //intro variables
+    private String snakeColor;
+    private String backgroundColor;
+    private String appleColor;
+    private int DELAY;
+    
+    //returned variables
+    long timeElapsed; 
+    int foodEAten;
+
 
     SnakePanel() {
         this.setPreferredSize(new Dimension(S_Width, S_Height));
@@ -42,6 +61,7 @@ public class SnakePanel extends JPanel implements ActionListener {
         game_flag = true;
         timer = new Timer(DELAY, this);
         timer.start();
+        gameTimer.startTimer();
     }
 
     public void paintComponent(Graphics graphic) {
@@ -49,6 +69,9 @@ public class SnakePanel extends JPanel implements ActionListener {
         draw(graphic);
     }
 
+    
+    //Snake speed
+    //snake color, background color, apple color
     public void draw(Graphics graphic) {
         if (game_flag) {
             graphic.setColor(Color.yellow);
@@ -144,6 +167,9 @@ public class SnakePanel extends JPanel implements ActionListener {
         graphic.setFont(new Font("Ink Free", Font.BOLD, 40));
         FontMetrics font_me3 = getFontMetrics(graphic.getFont());
         graphic.drawString("Press R to Replay", (S_Width - font_me3.stringWidth("Press R to Replay")) / 2, S_Height / 2 - 150);
+        gameTimer.stopTimer();
+        //test
+        System.out.println(gameTimer.getTimeElapsed());
     }
 
     public class MyKey extends KeyAdapter {
@@ -195,4 +221,87 @@ public class SnakePanel extends JPanel implements ActionListener {
         repaint();
     }
 
+    public int getBodylength() {
+        return bodylength;
+    }
+
+    public void setBodylength(int bodylength) {
+        this.bodylength = bodylength;
+    }
+
+    public int getFoodX() {
+        return foodX;
+    }
+
+    public void setFoodX(int foodX) {
+        this.foodX = foodX;
+    }
+
+    public int getFoodY() {
+        return foodY;
+    }
+
+    public void setFoodY(int foodY) {
+        this.foodY = foodY;
+    }
+
+    public boolean isGame_flag() {
+        return game_flag;
+    }
+
+    public void setGame_flag(boolean game_flag) {
+        this.game_flag = game_flag;
+    }
+
+    public int getFoodEAten() {
+        return foodEAten;
+    }
+
+    public void setFoodEAten(int foodEAten) {
+        this.foodEAten = foodEAten;
+    }
+
+    public String getSnakeColor() {
+        return snakeColor;
+    }
+
+    public void setSnakeColor(String snakeColor) {
+        this.snakeColor = snakeColor;
+    }
+
+    public String getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(String backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public String getAppleColor() {
+        return appleColor;
+    }
+
+    public void setAppleColor(String appleColor) {
+        this.appleColor = appleColor;
+    }
+
+    public int getDELAY() {
+        return DELAY;
+    }
+
+    public void setDELAY(int DELAY) {
+        this.DELAY = DELAY;
+    }
+
+    public long getTimeElapsed() {
+        return timeElapsed;
+    }
+
+    public void setTimeElapsed(long timeElapsed) {
+        this.timeElapsed = timeElapsed;
+    }
+
+    
+    
+    
 }
